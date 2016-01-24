76eeb711985e78f696e2784e09dd92799864e3a7
Aokashi <Aokashi.Towa@Gmail.com>
2016-01-25 00:54:58 +0900

    1行ずつ1つの文字列を選定してくっつけるように仕様変更、確率も付けられる

--- makename_jpn.js
+++ makename_jpn.js
@@ -1,31 +1,37 @@
 function Make_name() {
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-    	createArray(xhr.responseText);
-    };
-    xhr.open("get", "jpn_lib.csv", true);
-    xhr.send(null);
+	var xhr = new XMLHttpRequest();
+	xhr.onload = function() {
+		createArray(xhr.responseText);
+	};
+	xhr.open("get", "jpn_lib.csv", true);
+	xhr.send(null);
 }
 function createArray(csvData) {
+	var csvArray = new Array();
 	var tempArray = csvData.split("\n");
-	var firstArray = tempArray[0].split(',');
-	var lastArray = tempArray[1].split(',');
-    console.log(tempArray);
-    var output_num = document.name_form.o_num.value;
-	document.name_form.result.value = '';
-	var i, j;
-    // 乱数命名
-	var fname, lname, fnum, lnum;
-	for (i = 0; i < output_num; i++) {
+	var i;
+	for(i = 0; i < tempArray.length; i++){
+		csvArray[i] = tempArray[i].split(",");
+	}
+	var output_num = document.name_form.o_num.value;
+	document.name_form.result.value = "";
+	var j;
+	var radd, rname, rnum;
+	for (i = 0; i < output_num; i++) { // iは回数ごと
 		if (document.name_form.add_no.checked) { // Noをつけた場合
-			document.name_form.result.value += (i + 1) + '：';
+			document.name_form.result.value += (i + 1) + "：";
 		}
-		fname = ""; // 最初のところ
-		fnum = Math.floor(Math.random() * firstArray.length);
-		fname += firstArray[fnum];
-		lname = ""; // 後のところ
-		lnum = Math.floor(Math.random() * lastArray.length);
-		lname += lastArray[lnum];
-		document.name_form.result.value += fname + lname + "\n";
+		for(j = 0; j < csvArray.length; j++){ // jは行ごと
+			radd = Math.floor(Math.random() * 100) + 1;
+			if(radd > csvArray[j][0]){ // 確率で付けるか判定
+				continue;
+			}
+			rnum = Math.floor(Math.random() * (csvArray[j].length - 1)) + 1; // 選定
+			rname = csvArray[j][rnum];
+			document.name_form.result.value += rname.trim(); // くっつける
+		}
+		if(i + 1 < output_num){ // 最後でない場合は改行を付ける
+			document.name_form.result.value += "\n";
+		}
 	}
 }
